be2f991e3e754ce407d34cc7caaf6c85582b8f2e
Jim Fowler <fowler@math.osu.edu>
2022-07-14 09:06:37 -0400

    using free_group but with some sorrys

--- src/word_metric.lean
+++ src/word_metric.lean
@@ -41,13 +41,30 @@ end
 
 def words (g : G) : set (free_group GG.gens) := { w : free_group GG.gens | of GG w = g }
 
-def word_norm (g : G) : nat := Inf $ list.length '' (free_group.to_word '' (words GG g))
+def words_nonempty (g : G) : (words GG g).nonempty := begin
+  have h : ∃ w : free_group GG.gens, w ∈ words GG g, {
+    let hk := GG.closure_eq_top,
 
+    let f := free_group.lift (id : GG.gens → GG.gens),
+
+    rw [← free_group.lift.range_eq_closure, monoid_hom.range_top_iff_surjective ],
+    intro x, use x,
+    exact free_group.lift.of_eq x,
+    
+    sorry,
+  },
+  exact h,
+end  
+
+-- should be in terms of has_norm
+def word_norm (g : G) : nat := Inf $ (list.length ∘ free_group.to_word) '' (words GG g)
+
+-- this is surely much too long
 lemma norm_one_eq_zero : word_norm GG (1 : G) = 0 :=
 begin
   set ws := words GG 1 with hws,
   
-  suffices : 0 ∈ list.length '' (free_group.to_word '' ws),
+  suffices : 0 ∈ (list.length ∘ free_group.to_word) '' ws,
   { 
     rw [← nonpos_iff_eq_zero],
     refine nat.Inf_le this , 
@@ -72,22 +89,112 @@ end
 
 lemma norm_eq_zero (g : G) : word_norm GG g = 0 → g = 1 :=
 begin
-  sorry
+  intro h,
+   
+  let lw := (list.length ∘ free_group.to_word) '' (words GG g),
+  have hl : set.nonempty lw := begin
+    simp only [set.nonempty_image_iff],
+    exact words_nonempty GG g,
+  end,
+  
+  have hk := nat.Inf_mem hl,
+  unfold word_norm at h,
+  rw h at hk,
+  simp only [set.mem_image, exists_exists_and_eq_and] at hk,
+  obtain ⟨ w, h2, h3 ⟩ := hk,
+  unfold words at h2,
+  simp only [set.mem_set_of_eq] at h2,
+  rw list.length_eq_zero at h3,
+
+  have h4 : (1 : free_group GG.gens).to_word = list.nil, {
+    unfold free_group.to_word,
+    unfold has_one.one,
+    rw ← free_group.red.nil_iff,
+    simp only [free_group.quot_lift_mk],
+    rw free_group.red.nil_iff ,
+    unfold free_group.reduce,
+  },
+  
+  rw ← h3 at h4,
+
+  have h5 : w = 1, {
+    apply free_group.to_word.inj,
+    assumption,
+  },
+
+  rw h5 at h2,
+
+  unfold of at h2,  
+  simp only [map_one] at h2,
+  exact h2.symm, 
 end
 
 @[simp] lemma norm_zero_iff_one (g : G) : word_norm GG g = 0 ↔ g = 1 :=
 begin
-  sorry
+  refine ⟨ norm_eq_zero GG g, _ ⟩,
+  rintro rfl,
+  apply norm_one_eq_zero,
 end
+
+theorem nat.Inf_le_Inf {s t : set ℕ} (hs : s.nonempty) (ht : s ⊆ t) : Inf t ≤ Inf s 
+:= nat.Inf_le $ ht $ nat.Inf_mem hs
 
 lemma word_norm_le_inv (x : G) : word_norm GG x⁻¹ ≤ word_norm GG x :=
 begin
-  sorry
+  set ws := words GG x with hws,
+  set wi := words GG x⁻¹ with hwi,
+  
+  let lw := (list.length ∘ free_group.to_word) '' ws,
+  have k : lw ⊆ (list.length ∘ free_group.to_word) '' wi),
+  {
+    intros lp h,
+    simp only [set.mem_image, exists_exists_and_eq_and] at h,
+    obtain ⟨ p , hp, hl ⟩ := h,
+    simp,
+  
+    use p⁻¹,
+
+    split,
+    { 
+       rw hws at hp,
+       unfold words at hp,
+       simp at hp,
+       unfold of at hp,
+       
+       rw hwi,
+       unfold words,
+       simp only [set.mem_set_of_eq],
+       unfold of,
+
+       simp only [map_inv, inv_inj] at hp ⊢,
+       assumption,
+    }, 
+    {
+      rw ← hl,
+      simp,
+      sorry,
+    },
+  },
+  unfold word_norm at *,
+
+  have hl : set.nonempty lw := begin
+    simp only [set.nonempty_image_iff],
+    exact words_nonempty GG x,
+  end,
+  
+  exact nat.Inf_le_Inf hl k, 
 end
 
 lemma word_norm_symm (x : G) : word_norm GG x = word_norm GG x⁻¹ :=
 begin
-  sorry
+  have lt : word_norm GG x⁻¹ ≤ word_norm GG x := word_norm_le_inv GG x,
+  apply le_antisymm,
+  { 
+    have h : word_norm GG x⁻¹⁻¹ ≤ word_norm GG x⁻¹ := word_norm_le_inv GG x⁻¹,
+    simp at h,
+    assumption,
+  },
+  { assumption, },
 end
 
 def word_dist (x y : G) : nat := word_norm GG (x * y⁻¹)
@@ -120,7 +227,40 @@ end
 
 def word_norm_triangle (x y : G) : word_norm GG (x * y) ≤ word_norm GG x + word_norm GG y :=
 begin
-  sorry
+  set wx := words GG x with hwx,
+  set wy := words GG y with hwy,
+  set wxy := words GG (x * y) with hwxy,
+
+  let lx := (list.length ∘ free_group.to_word) '' wx,
+  have hlx : set.nonempty lx := begin
+    simp only [set.nonempty_image_iff],
+    exact words_nonempty GG x,
+  end,
+
+  let ly := (list.length ∘ free_group.to_word) '' wy,
+  have hly : set.nonempty ly := begin
+    simp only [set.nonempty_image_iff],
+    exact words_nonempty GG y,
+  end,
+
+  rcases nat.Inf_mem hlx with ⟨ px, hx2, hx3 ⟩,
+  rcases nat.Inf_mem hly with ⟨ py, hy2, hy3 ⟩,
+
+  have h : px * py ∈ words GG (x * y) := begin
+    sorry,
+  end,
+
+  unfold word_norm,
+  rw [←hx3, ←hy3],
+        
+  have k : Inf ((list.length ∘ free_group.to_word) '' (words GG (x * y))) ≤ (list.length ∘ free_group.to_word) (px * py) := begin
+    sorry,
+  end, 
+  have k2 : (list.length ∘ free_group.to_word) (px * py) ≤ (list.length ∘ free_group.to_word) px + (list.length ∘ free_group.to_word) py := begin
+    simp,
+    sorry, 
+  end,
+  finish,
 end
 
 def word_dist_triangle : ∀ (x y z : G), word_dist GG x z ≤ word_dist GG x y + word_dist GG y z :=
